e5e8dfee51cd2a96e51919d88097c1f54a9688f3
rycao18stu <vanguardsmurf8@gmail.com>
2017-11-29 08:00:23 -0600

    woopsies it expands the GUI

--- WebGet/ChatGUI.java
+++ WebGet/ChatGUI.java
@@ -11,6 +11,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.IOException;
 import java.util.Scanner;
+import java.io.PrintWriter;
 
 /**
  * Write a description of class ChatGUI here.
@@ -18,54 +19,68 @@ import java.util.Scanner;
  * @author (your name)
  * @version (a version number or a date)
  */
-public class ChatGUI implements ActionListener, Runnable{
+public class ChatGUI implements ActionListener{
     JFrame frame;
     JTextArea display;
-    JTextField entry;
+    JTextField entry, username;
     JButton submit;
-    OutputStream out;
+    OutputStream outstream;
     InputStream in;
     Socket s;
-    public ChatGUI() {
+    PrintWriter out;
+    Scanner scanner;
+
+    public ChatGUI() throws IOException, InterruptedException {
         this.frame = new JFrame("ChatGUI");
-        this.display = new JTextArea(50, 50);
+        this.display = new JTextArea(30, 50);
         this.display.setEnabled(false);
         this.display.setLineWrap(true);
         this.frame.add(display, BorderLayout.NORTH);
         this.entry = new JTextField(50);
         this.frame.add(entry, BorderLayout.CENTER);
+        this.username = new JTextField(10);
+        this.frame.add(username, BorderLayout.WEST);
         this.submit = new JButton("Send");
         this.submit.addActionListener(this);
         this.frame.add(submit, BorderLayout.EAST);
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.frame.setVisible(true);
-        this.frame.setSize(new Dimension(1000, 1000));
-        this.s = new Socket(
+        this.frame.setSize(new Dimension(600, 500));
+        this.s = new Socket("192.168.1.3", 8081);
+        this.in = s.getInputStream();
+        this.outstream = s.getOutputStream();
+        this.out = new PrintWriter(this.outstream);
+        this.scanner = new Scanner(this.in);
+        this.scanner.useDelimiter("qwert");
     }
-    
-    public static void main(String[] args) {
-        new ChatGUI();
-    }
-    
-    public void actionPerformed(ActionEvent e) {
-        this.display.append(entry.getText() + "\n");
-        try {
-            this.out.write(this.display.getText().getBytes());
-        } catch (Exception ee) {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ChatGUI x = new ChatGUI();
+        while (true) {
+            try {
+                x.update();
+                Thread.sleep(50);
+            } catch (Exception e) {
+                System.out.println("1" + e);
+                Thread.sleep(50);
+            }
         }
     }
-    
-    public void run(){
-        Scanner s = new Scanner(in);
-        s.useDelimiter("SA5DFHAS5DIFHASDOIFH55665340093cxlkv328068EOIROIRWEOIHRWERWE");
-        while(true) {
-            if (s.hasNext()) {
-                this.display.setText(s.next());
-            }
-            try {
-                Thread.sleep(50);
-            } catch (InterruptedException e) {
-            }
+
+    public void actionPerformed(ActionEvent e) {
+        this.display.setText("");
+        this.display.append(this.username.getText() + ": " + this.entry.getText() + "\n");
+        try {
+            this.out.print(this.display.getText());
+            this.out.flush();
+        } catch (Exception ee) {
+            System.out.println("2" + ee);
+        }
+    }
+
+    public void update() throws IOException{
+        if (in.available() > 0) {
+            this.display.setText(this.scanner.next());
         }
     }
 }
